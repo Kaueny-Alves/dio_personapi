45dff05e4342382215d730e94fcf3253c88e97b9
Kaueny de Oliveira Alves <kaueny.alves@raizen.com>
2021-08-12 15:14:40 -0300

    Developed delete By Id

--- src/main/java/com/example/kaueny/personapi/controller/PersonController.java
+++ src/main/java/com/example/kaueny/personapi/controller/PersonController.java
@@ -7,6 +7,7 @@ import com.example.kaueny.personapi.dto.response.MessageResponseDTO;
 import com.example.kaueny.personapi.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,11 @@ public class PersonController {
   @GetMapping("/{id}")
   public  PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
     return personService.findById(id);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void DeleteById(@PathVariable Long id) throws PersonNotFoundException {
+    personService.delete(id);
   }
 }
--- src/main/java/com/example/kaueny/personapi/service/PersonService.java
+++ src/main/java/com/example/kaueny/personapi/service/PersonService.java
@@ -42,10 +42,17 @@ public class PersonService {
   }
 
   public PersonDTO findById(Long id) throws PersonNotFoundException {
-
-   Person person = personRepository.findById(id)
-       .orElseThrow(() -> new PersonNotFoundException(id));
-
+   Person person = verifyIdExists(id);
     return personMapper.toDTO(person);
+  }
+
+  private Person verifyIdExists(Long id) throws PersonNotFoundException {
+    return personRepository.findById(id)
+        .orElseThrow(() -> new PersonNotFoundException(id));
+  }
+
+  public void delete(Long id) throws PersonNotFoundException {
+    verifyIdExists(id);
+    personRepository.deleteById(id);
   }
 }
